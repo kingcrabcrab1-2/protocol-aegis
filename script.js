23aa6a35e7ece6eaa79f118ed15a7518ddcaf95d
kingcrabcrab1-2 <kingcrabcrab.1@gmail.com>
2026-07-21 01:10:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -428,21 +428,22 @@ $("adminToggle").addEventListener(
     
 try {
 
-  await api("/api/admin/verify", {
-    method: "POST",
-    headers: {
-      "X-Admin-Password": pw
+      await api("/api/admin/verify", {
+        method: "POST",
+        headers: {
+          "X-Admin-Password": pw
+        }
+      });
+
+    } catch {
+
+      $("adminStatus").textContent =
+        "비밀번호가 올바르지 않습니다.";
+
+      return;
+
     }
-  });
 
-} catch {
-
-  $("adminStatus").textContent =
-    "비밀번호가 올바르지 않습니다.";
-
-  return;
-
-}
     adminPassword = pw;
     adminMode = true;
 
