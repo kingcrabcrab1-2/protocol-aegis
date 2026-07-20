a60fc04149d5d8a072b62c964a0a8f486bd018a7
kingcrabcrab1-2 <kingcrabcrab.1@gmail.com>
2026-07-20 22:13:36 +0900

    Delete functions/api directory

--- functions/api/admin/verify.js
+++
@@ -1,5 +0,0 @@
-import { json, isAdmin, unauthorized } from "../../_shared.js";
-
-export async function onRequestPost({ request, env }) {
-  return isAdmin(request, env) ? json({ ok: true }) : unauthorized();
-}
